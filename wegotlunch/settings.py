874d20f9cc287f6344abec129af09d3c1ea8734c
tadaskrisciunas <tadaskrisciunas@gmail.com>
2018-07-19 11:30:17 +0100

    Restoring old location.

--- wegotlunch/settings.py
+++ wegotlunch/settings.py
@@ -4,7 +4,7 @@ Settings of the WeGotLunch app.
 (c) Tadas Krisciunas, 2018.
 """
 
-DATA_LOCATION = '/home/zopadev/Food_Hero/wegotlunch_backend/data/food.pkl'
+DATA_LOCATION = 'C:/Dev/wegotlunch_backend/data/food.pkl'
 """
 The location where our data sits.
 """
